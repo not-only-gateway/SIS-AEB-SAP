7a1ff9ddc2b634a9605dc4abd3545fea0044fcec
facobackup <gustavomicaelbarbosa@gmail.com>
2021-03-29 19:56:11 -0300

    Collaboration functions

--- components/forms/person/collaborator/CollaborationForm.js
+++ components/forms/person/collaborator/CollaborationForm.js
@@ -21,18 +21,53 @@ export default class CollaborationForm extends React.Component {
             publicationDate: null,
             admissionDate: null,
             legalDocument: null,
-            activeRole: true,
+            activeRole: null,
             unityID: null,
             roleID: null,
             linkageID: null,
-            substitute: false,
-            origin: null
+            substitute: null,
+            origin: null,
+            changed: false,
+            roleLevel: null
         }
+        this.handleChange = this.handleChange.bind(this)
+        this.handleDateChange = this.handleDateChange.bind(this)
+        this.handleRoleChange = this.handleRoleChange.bind(this)
     }
 
     componentDidMount() {
         this.fetchData().catch(error => console.log(error))
     }
+    handleRoleChange(event){
+        this.setState({
+            roleID: event.roleID,
+            roleLevel: event.roleLevel
+        }, () => this.fetchSeniors(event.roleLevel).catch(error => console.log(error)))
+        if(!this.state.changed)
+            this.setState({
+                changed: true
+            })
+
+    }
+    handleChange(event){
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+        if(!this.state.changed)
+            this.setState({
+                changed: true
+            })
+    }
+    handleDateChange(name,event) {
+        this.setState({
+            [name]: event.getTime()
+        })
+        if(!this.state.changed)
+            this.setState({
+                changed: true
+            })
+    }
+
 
     async fetchData() {
         try {
@@ -100,13 +135,27 @@ export default class CollaborationForm extends React.Component {
     }
 
     async fetchSeniors() {
-
+        try{
+            await axios({
+                method: 'get',
+                url: Host() + 'seniors',
+                headers: {'authorization': cookies.get('jwt')},
+                params: {
+                    level: this.state.roleLevel,
+                    unity_id: this.state.unityID
+                }
+            }).then(res => {
+                this.setState({
+                    seniors: res.data
+                })
+            }).catch(error => {
+                console.log(error)
+            })
+        }catch (error){
+            console.log(error)
+        }
     }
 
-    handleChangeUnity(id) {
-        this.setState({unityID: id})
-        this.fetchSeniors().catch(error => console.log(error))
-    }
 
     async saveChanges() {
         try {
@@ -130,7 +179,8 @@ export default class CollaborationForm extends React.Component {
                         labelId="unity-select"
                         id="unity-select"
                         value={this.state.unityID}
-                        onChange={event => this.handleChangeUnity(event.target.value)}
+                        name={'unityID'}
+                        onChange={this.handleChange}
                         label="Unity"
                     >
                         {this.state.unities.map(unity => (
@@ -138,15 +188,17 @@ export default class CollaborationForm extends React.Component {
                         ))}
                     </Select>
                 </FormControl>
-                <FormControl variant="outlined" disabled={this.props.disabled}
+                <FormControl variant="outlined" disabled={this.props.disabled || this.state.seniors.length === 0}
                              style={{...this.props.selectStyle, ...{width: '49%'}}}>
                     <InputLabel id="senior-select">Senior</InputLabel>
                     <Select
+
                         labelId="senior-select"
                         id="senior-select"
                         value={this.state.seniorID}
                         disabled={this.state.seniorID === null}
-                        // onChange={handleChange}
+                        name={'seniorID'}
+                        onChange={this.handleChange}
                         label="Senior"
                     >
                         {this.state.seniors.map(senior => (
@@ -154,29 +206,31 @@ export default class CollaborationForm extends React.Component {
                         ))}
                     </Select>
                 </FormControl>
-                <FormControl variant="outlined" disabled={this.props.disabled}
+                <FormControl variant="outlined" disabled={this.props.disabled || this.state.unityID === null}
                              style={{...this.props.selectStyle, ...{width: '49%'}}}>
                     <InputLabel id="role-select">Role</InputLabel>
                     <Select
                         labelId="role-select"
                         id="role-select"
-                        value={this.state.roleID}
-                        // onChange={handleChange}
+                        value={this.state.roleID !== null ? JSON.stringify({roleID: this.state.roleID, roleLevel: this.state.roleLevel}) : null}
+                        // name={'roleID'}
+                        onChange={event => this.handleRoleChange(JSON.parse(event.target.value))}
                         label="role"
                     >
                         {this.state.roles.map(role => (
-                            <MenuItem value={role.id}>{role.description} - {role.level}</MenuItem>
+                            <MenuItem value={JSON.stringify({roleID: role.id, roleLevel: role.level})}>{role.denomination} - {role.is_effective ? 'Effective' : 'Not Effective'}</MenuItem>
                         ))}
                     </Select>
                 </FormControl>
-                <FormControl variant="outlined" disabled={this.props.disabled}
+                <FormControl variant="outlined" disabled={this.props.disabled || this.state.unityID === null}
                              style={{...this.props.selectStyle, ...{width: '49%'}}}>
                     <InputLabel id="active-role-select">Active on role</InputLabel>
                     <Select
                         labelId="active-role-select"
                         id="active-role-select"
                         value={this.state.activeRole}
-                        // onChange={handleChange}
+                        name={'activeRole'}
+                        onChange={this.handleChange}
                         label="Active Role"
                     >
                         <MenuItem value={true}>Yes</MenuItem>
@@ -195,11 +249,12 @@ export default class CollaborationForm extends React.Component {
                             inputVariant="outlined"
                             margin="normal"
                             id="admission-picker"
-                            disabled={this.props.disabled}
+                            disabled={this.props.disabled || this.state.unityID === null}
                             label="Admission Date"
                             format="dd/MM/yyyy"
                             value={this.state.admissionDate !== null ? new Date(this.state.admissionDate).toLocaleDateString() : null}
-                            // onChange={handleDateChange}
+                            name={'admissionDate'}
+                            onChange={event => this.handleDateChange('admissionDate', event)}
                             KeyboardButtonProps={{
                                 'aria-label': 'change date',
                             }}
@@ -219,45 +274,50 @@ export default class CollaborationForm extends React.Component {
                             inputVariant="outlined"
                             margin="normal"
                             id="publication-picker"
-                            disabled={this.props.disabled}
+                            disabled={this.props.disabled || this.state.unityID === null}
                             label="Official Publication"
                             format="dd/MM/yyyy"
                             value={this.state.publicationDate !== null ? new Date(this.state.publicationDate).toLocaleDateString() : null}
-                            // onChange={handleDateChange}
+                            onChange={event => this.handleDateChange('publicationDate', event)}
                             KeyboardButtonProps={{
                                 'aria-label': 'change date',
                             }}
                         />
                     </Grid>
                 </MuiPickersUtilsProvider>
-                <FormControl variant="outlined" disabled={this.props.disabled} style={this.props.selectStyle}>
+                <FormControl variant="outlined" disabled={this.props.disabled || this.state.unityID === null || this.state.roleID === null} style={this.props.selectStyle}>
                     <InputLabel id="link-select">Linkage</InputLabel>
                     <Select
                         labelId="link-select"
                         id="link-select"
-                        value={false}
-                        // onChange={handleChange}
+                        value={this.state.linkageID}
+                        name={'linkageID'}
+                        onChange={this.handleChange}
                         label="Linkage"
                     >
-                        <MenuItem value={true}>Yes</MenuItem>
-                        <MenuItem value={false}>No</MenuItem>
+                        {this.state.linkages.map(link => (
+                            <MenuItem value={link.id}>{link.description}</MenuItem>
+                        ))}
                     </Select>
                 </FormControl>
-                <FormControl variant="outlined" disabled={this.props.disabled} style={{...this.props.selectStyle, ...{width: '49%'}}}>
+                <FormControl variant="outlined" disabled={this.props.disabled || this.state.unityID === null || this.state.roleID === null} style={{...this.props.selectStyle, ...{width: '49%'}}}>
                     <InputLabel id="substitute-select">Substitute</InputLabel>
                     <Select
                         labelId="substitute-select"
                         id="substitute-select"
                         value={this.state.substitute}
-                        // onChange={handleChange}
+                        name={'substitute'}
+                        onChange={this.handleChange}
                         label='Substitute'
                     >
                         <MenuItem value={true}>Yes</MenuItem>
                         <MenuItem value={false}>No</MenuItem>
                     </Select>
                 </FormControl>
-                <TextField disabled={this.props.disabled} label={'Legal Document'} value={this.props.name}
+                <TextField disabled={this.props.disabled || this.state.unityID === null} label={'Legal Document'} value={this.state.legalDocument}
                            variant={"outlined"}
+                           name={'legalDocument'}
+                           onChange={this.handleChange}
                            style={this.props.mediumContainer}/>
 
                 <Button style={{width: '100%'}}>Save changes</Button>
