1cb00cbf1bac45c47a0033aadffa835ecf48157f
facobackup <gustavomicaelbarbosa@gmail.com>
2021-06-19 22:16:31 -0300

    New navbar

--- apps/admin/components/shared/layout/test/Navigation.js
+++ apps/admin/components/shared/layout/test/Navigation.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import styles from './styles/Navigation.module.css'
 import PropTypes from 'prop-types'
 
@@ -6,60 +6,54 @@ import NavigationProfile from './templates/NavigationProfile'
 import NavigationButton from './templates/NavigationButton'
 import NavigationPT from './locales/NavigationPT'
 import NavigationApps from './templates/NavigationApps'
+import {MenuRounded} from "@material-ui/icons";
+import NavigationTabs from "./templates/NavigationTabs";
 
 
 export default function Navigation(props) {
     const lang = NavigationPT
+    const [modal, setModal] = useState(false)
+
+
 
     return (
-        <div className={[styles.navigationContainer, props.loading ? styles.loading : ''].join(' ')}>
-            <div className={styles.logoContainer} style={{color: '#555555', fontWeight: 575}}>
+        <>
+            <div className={[styles.navigationContainer, props.loading ? styles.loading : ''].join(' ')}>
+                <div className={styles.logoContainer} style={{color: '#555555', fontWeight: 600, fontSize: '.95rem'}}>
+                    <NavigationTabs open={modal} setOpen={setModal} buttons={props.buttons}/>
+                    <button className={styles.buttonContainer} onClick={() => setModal(true)}>
+                        <MenuRounded/>
+                    </button>
+                    <img
+                        style={{height: '37px'}}
+                        src={props.logo} alt={'logo'}/>
+                    {props.appName}
 
-                <img
-                    style={{height: '50px'}}
-                    src={props.logo} alt={'logo'}/>
-                {props.appName}
 
+                </div>
+
+                <div className={styles.logoContainer} style={{justifyContent: 'flex-end', gap: '8px'}}>
+                    <NavigationApps lang={lang} buttons={props.apps}
+                                    centered={props.profile !== null && props.profile !== undefined}/>
+
+                    {props.profile !== null && props.profile !== undefined ?
+                        <NavigationProfile
+                            buttons={props.profileButtons}
+                            profile={{
+                                id: props.profile.id,
+                                image: props.profile.image,
+                                corporate_email: props.profile.corporate_email,
+                                name: props.profile.name
+                            }} reduced={props.reduced}
+                            setReduced={props.setReduced} accessProfile={props.accessProfile}
+                            lang={lang}/>
+                        :
+                        null
+                    }
+                </div>
 
             </div>
-            <div className={styles.logoContainer} style={{justifyContent: 'space-between', paddingLeft: '10%', paddingRight: '10%'}}>
-
-                {props.buttons.map((button, index) => (
-                    <NavigationButton
-                        buttonKey={index}
-                        linkPath={button.link}
-                        linkQuery={button.linkProps}
-                        highlight={props.path === button.link}
-                        icon={
-                            button.icon
-                        }
-                        label={button.label}
-                    />
-                    // </React.fragment>
-                ))}
-            </div>
-
-            <div className={styles.logoContainer} style={{justifyContent: 'flex-end', gap: '8px'}}>
-                <NavigationApps lang={lang} buttons={props.apps}
-                                centered={props.profile !== null && props.profile !== undefined}/>
-
-                {props.profile !== null && props.profile !== undefined ?
-                    <NavigationProfile
-                        buttons={props.profileButtons}
-                        profile={{
-                            id: props.profile.id,
-                            image: props.profile.image,
-                            corporate_email: props.profile.corporate_email,
-                            name: props.profile.name
-                        }} reduced={props.reduced}
-                        setReduced={props.setReduced} accessProfile={props.accessProfile}
-                        lang={lang}/>
-                    :
-                    null
-                }
-            </div>
-
-        </div>
+        </>
     )
 
 }
---
+++ apps/admin/components/shared/layout/test/templates/NavigationTabs.js
@@ -0,0 +1,82 @@
+import PropTypes from "prop-types";
+import React, {useEffect, useState} from "react";
+import animations from '../styles/Animations.module.css'
+import styles from "../styles/Navigation.module.css";
+import NavigationButton from "./NavigationButton";
+
+export default function NavigationTabs(props) {
+    const [isInRender, setIsInRender] = useState(false)
+    const [style, setStyle] = useState('')
+
+    useEffect(() => {
+        const subElement = document.querySelector('#navigation-content')
+        if (isInRender && !props.open) {
+            setStyle(animations.slideLeft)
+        } else if (!isInRender && props.open) {
+            setIsInRender(true)
+            setStyle(animations.slideRight)
+        }
+
+        if (subElement !== null)
+            subElement.addEventListener('animationend', () => {
+                if (style === animations.slideLeft && !props.open && isInRender) {
+                    setIsInRender(false)
+                    setStyle(animations.slideRight)
+                }
+
+            });
+
+
+
+        const element = document.getElementById('navigation-tabs')
+
+        if (element !== null && props.open)
+            element.addEventListener('mousedown', event => {
+                if (!subElement.contains(event.target))
+                    props.setOpen(false)
+            })
+    })
+
+
+    return (
+        <div id={'navigation-tabs'}
+             style={{
+                 width: '100vw',
+                 position: 'fixed',
+                 height: '100vh',
+                 display: isInRender ? undefined : 'none',
+                 zIndex: 100,
+                 bottom: 0
+             }}>
+            <div className={[styles.modalContainer, style].join(' ')} id={'navigation-content'}>
+                {props.buttons.map((button, index) => (
+                    <NavigationButton
+                        buttonKey={index}
+                        linkPath={button.link}
+                        linkQuery={button.linkProps}
+                        highlight={props.path === button.link}
+                        icon={
+                            button.icon
+                        }
+                        label={button.label}
+                    />
+                    // </React.fragment>
+                ))}
+            </div>
+        </div>
+    )
+
+}
+
+NavigationTabs.propTypes = {
+    buttons: PropTypes.arrayOf(
+        PropTypes.shape({
+            label: PropTypes.string,
+            icon: PropTypes.any,
+            link: PropTypes.string,
+            linkProps: PropTypes.any
+        })
+    ),
+    open: PropTypes.bool,
+    setOpen: PropTypes.func
+}
